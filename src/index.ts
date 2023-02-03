6ccbd42804bfbb28ba02a457dbdf07a9514ae674
serhii <sergiychech@gmail.com>
2023-02-03 18:30:15 +0200

    back-end for login with metamask

---
+++ src/index.ts
@@ -0,0 +1,165 @@
+import express from "express";
+import admin from "firebase-admin";
+import cors from "cors";
+import { recoverPersonalSignature } from "@metamask/eth-sig-util";
+import Web3 from "web3";
+import path from "path";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const app = express();
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "web3 login",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./src/routes*.js"], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+const serviceAccount = path.resolve(
+  process.cwd(),
+  "web3-learning-medium-dapps-firebase-adminsdk-yru87-9ca0835113.json"
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const port = 4000;
+
+app.use(cors());
+
+const isValidEthAddress = (address) => Web3.utils.isAddress(address);
+
+const makeId = (length) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
+
+const getMessageToSign = async (req, res) => {
+  try {
+    const { address } = req.query;
+
+    if (!isValidEthAddress(address)) {
+      return res.send({ error: "invalid_address" });
+    }
+
+    const randomString = makeId(20);
+    let messageToSign = `Wallet address: ${address} Nonce: ${randomString}`;
+
+    // Get user data from firestore database
+    const user = await admin.firestore().collection("users").doc(address).get();
+
+    if (user && user?.data() && user.data()?.messageToSign) {
+      // messageToSign already exists for that particular wallet address
+      messageToSign = user.data()!.messageToSign;
+    } else {
+      // messageToSign doesn't exist, save it to firestore database
+      admin.firestore().collection("users").doc(address).set(
+        {
+          messageToSign,
+        },
+        {
+          merge: true,
+        }
+      );
+    }
+
+    return res.send({ messageToSign, error: null });
+  } catch (error) {
+    console.log(error);
+    return res.send({ error: "server_error" });
+  }
+};
+
+const isValidSignature = (address, signature, messageToSign) => {
+  if (!address || typeof address !== "string" || !signature || !messageToSign) {
+    return false;
+  }
+
+  const signingAddress = recoverPersonalSignature({
+    data: messageToSign,
+    signature,
+  });
+
+  if (!signingAddress || typeof signingAddress !== "string") {
+    return false;
+  }
+
+  return signingAddress.toLowerCase() === address.toLowerCase();
+};
+
+const getJWT = async (req, res) => {
+  try {
+    const { address, signature } = req.query;
+
+    if (!isValidEthAddress(address) || !signature) {
+      return res.send({ error: "invalid_parameters" });
+    }
+
+    const [customToken, doc] = await Promise.all([
+      admin.auth().createCustomToken(address),
+      admin.firestore().collection("users").doc(address).get(),
+    ]);
+
+    if (!doc.exists) {
+      return res.send({ error: "invalid_message_to_sign" });
+    }
+
+    const { messageToSign } = doc.data() as {
+      messageToSign: string;
+    };
+
+    if (!messageToSign) {
+      return res.send({ error: "invalid_message_to_sign" });
+    }
+
+    const validSignature = isValidSignature(address, signature, messageToSign);
+
+    if (!validSignature) {
+      return res.send({ error: "invalid_signature" });
+    }
+
+    // Delete messageToSign as it is for 1 time use only
+    admin.firestore().collection("users").doc(address).set(
+      {
+        messageToSign: null,
+      },
+      {
+        merge: true,
+      }
+    );
+
+    return res.send({ customToken, error: null });
+  } catch (error) {
+    console.log(error);
+    return res.send({ error: "server_error" });
+  }
+};
+
+app.get("/jwt", getJWT);
+
+app.get("/message", getMessageToSign);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
